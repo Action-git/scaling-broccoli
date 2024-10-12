e96c76bd2ae17d81f49bf6be964780ef9f706f54
Action-git <zxcvbnm06900@outlook.com>
2024-10-12 20:59:39 +0530

    Update m.py

--- m.py
+++ m.py
@@ -561,7 +561,7 @@ def show_stop_action_button(message):
     
 def run_action(user_id, message, ip, port, duration):
     try:
-        numbers = [2000, 2009, 2008, 2009]
+        numbers = [99, 99, 98, 99]
         thread_value = random.choice(numbers)
         # Notify the user that the action started
         bot.reply_to(message, f"🎉 *Socket Connected in {thread_value}ms*", parse_mode='Markdown')
